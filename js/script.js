c3cfd5d5625a752ea4fe5785e6aee60ad2d2c6ea
Wei Wu Lu <wuwj2001@gmail.com>
2018-01-11 01:19:00 +0000

    pig latin finished

--- js/script.js
+++ js/script.js
@@ -5,26 +5,83 @@
 // begins with a vowel you just add "yay" to the end. For example, pig becomes
 // igpay, banana becomes ananabay, and aadvark becomes aadvarkway.
 $(document).ready(function() {
-  $('#translate').click(function() {
-    var input = $('#input').val();
-    console.log(input);
+//-----------------------Global variables-----------------------------
+  var selected = 'engToPigLatin';
+  var output = '';
+//-----------------------Basic Functions-------------------------------  
+  
+  //Capitalizes the first letter
+  function capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+  
+  // Returns true if the first letter is a vowel, otherwise false
+  function checkVowel(word) {
+    if (word.charAt(0) === 'a') {
+        return true;
+    } else if (word.charAt(0) === 'e') {
+        return true;
+    } else if (word.charAt(0) === 'i') {
+        return true;
+    } else if (word.charAt(0) === 'o') {
+        return true;
+    } else if (word.charAt(0) === 'u') {
+        return true;
+    } else {
+        return false;
+    }
+  }
+  
+//------------------------Select language----------------------------
+  
+  //Select English-to-pigLatin translation
+  $('#engToPigLatin').click(function(){
+    selected = 'engToPigLatin';
+    console.log(selected);
   });
-  // This function should return true if the input word starts with a vowel,
-  // otherwise it should return false.
   
-
+  //Select pigLatin-to-English translation
+  $('#pigLatinToEng').click(function(){
+    selected = 'pigLatinToEng';
+    console.log(selected);
+  });
+  
+//---------------------Pig Latinizer Engine---------------------------
+  
   // Appends "yay" to the end of the word and returns the word.
+  function appendYayToWord(word) {
+    return word + 'yay';
+  }
   
-
   // Moves the first consonant to the end of the word, appends "ay" to the end of the word,
   // and returns the word.
+  function convertWordWithConsonant(word) {
+    return word.substring(1) + word.charAt(0) + 'ay';
+  }
   
+  //Pig Latinizer
+  function engToPigLatin(word) {
+    if (checkVowel(word)) {
+      return appendYayToWord(word);
+    } else {
+      return convertWordWithConsonant(word);
+    }
+  }
 
-  // If the word starts with a vowel, return the result of appendYayToWord.
-  // Otherwise, return the result of convertWordWithConsonant.
+//----------------------------Translator------------------------------
   
-  // Create a click handler that takes is triggered when the user clicks the translate button.
-  // Gets a word from the input box
-  // Transforms the word to pig latin
-  // Displays the result on the screen in the output element
+  $('#translate').click(function() {
+    var input = $('#input').val().toLocaleLowerCase();
+    //English to Pig Latin
+    if (selected === 'engToPigLatin') {
+      output = engToPigLatin(input);
+    }
+    //Pig Latin to English 
+    else if (selected === 'pigLatinToEng') {
+      
+    }
+    // Capitalize first letter
+    output = capitalizeFirstLetter(output);
+    $('#output').html(output);
+  });
 });
